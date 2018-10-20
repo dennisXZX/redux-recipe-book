b16da404f7b297dda460ce8dfa767736ba58ef33
Dennis Xiao <dennisboys@gmail.com>
2018-10-20 12:01:23 +1000

    add middleware

--- store.js
+++ store.js
@@ -1,11 +1,13 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import logMiddleware from './middleware/log'
 import rootReducer from './reducers/root'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // create a store
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(logMiddleware)
+))
 
 // place the store into window global object
 window.store = store
